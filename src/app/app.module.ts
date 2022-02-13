be76bbd464f72c78088585d471f9f8edfe21613a
HenrryJK <henrryorbezo@upeu.edu.pe>
2022-02-12 21:23:08 -0500

    #3 Agregando AppState y configurando el StoreDevtools de redux

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 // NGRX
 import { StoreModule } from '@ngrx/store';
 import { todoReducer } from './todos/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 //----- ///
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
+import { environment } from 'src/environments/environment';
 
 
 @NgModule({
@@ -19,6 +21,11 @@ import { FooterComponent } from './footer/footer.component';
     BrowserModule,
     TodoModule,
    StoreModule.forRoot({ todos: todoReducer }),
+   StoreDevtoolsModule.instrument({
+    maxAge: 25,
+    logOnly: environment.production,
+  }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todos/todo.reducer.ts
+++ src/app/todos/todo.reducer.ts
@@ -3,8 +3,11 @@ import { Todo } from './models/todo.model';
 import { crear } from './todo.actions';
 
 // el estaod inicial va a trabajar con un arreglo de la clase Todo que es un modelo (una clase) por lo tanto se le pone "[]" porque de paso esta vacio
-export const estadoInicial:Todo[] = [];
+// export const estadoInicial:Todo[] = [];
 
+export const estadoInicial:Todo[] = [
+  new Todo('Salvar al mundo')
+];
 const _todoReducer = createReducer(
   estadoInicial,
   // ...state , lo que hace es extraer y separar  cada uno de los items y regresarlos de manera independiente
