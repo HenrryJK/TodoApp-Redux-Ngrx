07314b6bfda29a400f63e287bd3b34361075be64
HenrryJK <henrryorbezo@upeu.edu.pe>
2022-03-08 15:25:20 -0500

    #11 52. Combinando 2 o más reducers en el AppState

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from "@angular/forms";
 // NGRX
 import { StoreModule } from '@ngrx/store';
-import { todoReducer } from './todos/todo.reducer';
+// import { todoReducer } from './todos/todo.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 //----- ///
 import { AppComponent } from './app.component';
 import { TodoModule } from './todos/todo.module';
 import { FooterComponent } from './footer/footer.component';
 import { environment } from 'src/environments/environment';
+import { appReducers } from './app.reducer';
 
 
 @NgModule({
@@ -23,7 +24,9 @@ import { environment } from 'src/environments/environment';
     ReactiveFormsModule,
     TodoModule,
     // como hace una conexion con un modulo independiente
-   StoreModule.forRoot({ todos: todoReducer }),
+  // StoreModule.forRoot({ todos: todoReducer }),
+  // se realizo un cambio para poder entender q lo que estabamos llamando era un objecto reducer , pero en este caso se ha creado una constante general q contiene tanto el listado de "todos" y los "filtros"
+  StoreModule.forRoot(appReducers),
    StoreDevtoolsModule.instrument({
     maxAge: 25,
     logOnly: environment.production,
